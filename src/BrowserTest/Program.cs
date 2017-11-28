d6bb12adb90fba21890eab06e28d1315103a0027
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-11-28 12:41:01 -0800

    initial project

---
+++ src/BrowserTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BrowserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
